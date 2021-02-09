a77068705caa4d62866bfe5cde3ca218ae2fa66e
Maixent Chenebaux <max.chbx@gmail.com>
2021-02-09 14:58:43 +0100

    changed link vectorizer to split _-/

--- xmlv/vectorizer.py
+++ xmlv/vectorizer.py
@@ -1,5 +1,5 @@
 from sklearn.feature_extraction.text import CountVectorizer
-
+import re
 
 def fit_transform(series, min_df=1, tokenize=False):
     if not tokenize:
@@ -16,7 +16,10 @@ def fit_transform(series, min_df=1, tokenize=False):
 def link_fit_transform(series, min_df=1):
     vectorizer = CountVectorizer(
         min_df=min_df,
-        preprocessor=lambda x: x.split("/"), tokenizer=lambda x: x)
+        preprocessor=lambda x: [
+            e for e in re.split(r'[\/_-]', x) if len(e) > 0
+        ],
+        tokenizer=lambda x: x)
     X = vectorizer.fit_transform(series).todense()
     return X, vectorizer
 
