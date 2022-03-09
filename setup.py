1e051e6bf3935124e0ba5e178e29e26de1d834c6
Maixent Chenebaux <max.chbx@gmail.com>
2022-03-09 10:17:53 +0100

    v0.0.2

--- setup.py
+++ setup.py
@@ -1,13 +1,13 @@
 import setuptools
 
-
 setuptools.setup(
     name="xmlv",
-    version="0.0.1",
+    version="0.0.2",
     author="Maixent Chenebaux",
     author_email="max.chbx@gmail.com",
     description="XMLVectorizer turn elements of a web pages to numpy vectors",
     packages=setuptools.find_packages(),
     include_package_data=True,
-    install_requires=["scikit-learn", "networkx", "pandas", "numpy", "dill"]
+    install_requires=["scikit-learn", "networkx",
+                      "pandas", "numpy", "dill", "rolewalk"]
 )
